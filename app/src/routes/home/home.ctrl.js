dbdc3afd47a50140bb83bd54a5c959ea22c5b1c5
myong-hoon <92171034+myong-hoon@users.noreply.github.com>
2021-10-16 23:00:38 +0900

    타입에러

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,29 @@ const output = {
   },
 };
 
+const users = {
+  id: ["1234", "123"],
+  pw: ["1234", "123"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      pw = req.body.pw;
+    console.log(id, pw);
+
+    if (users.id.includes(id)) {
+      const idx = users.id.index0f(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
